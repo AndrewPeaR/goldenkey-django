c3e4b9c298a423bf3a1d43cdbe5cbeb87ec7e322
AndrewPeaR <minecraft145236@gmail.com>
2025-03-26 15:26:01 +0500

    import site content

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Questions(models.Model):
+    name = models.TextField()
+    phoneNumber = models.TextField()
+    question = models.TextField()
+    status = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FAQ(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+
+class Docs(models.Model):
+    name = models.TextField()
+    doc = models.FileField(upload_to='uploads/docs')
+
+class News(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    image = models.FileField(upload_to='uploads/news')
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# from .models import Product
 
-# Create your views here.
+def index(request):
+    # products = Product.objects.all()
+    
+    context = {
+        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
+        'array': [1, 2, 3, 4],
+        # 'items': Product.objects.filter(name='server')
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return render(request, 'main/about.html')
