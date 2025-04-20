5d771aa69daa971ac93bd117f72931dfee8c84f8
Андрей <murzin.a@recifra.ru>
2025-04-20 18:04:32 +0500

    bleat

--- goldenkey/settings.py
+++ goldenkey/settings.py
@@ -30,10 +30,10 @@ SECRET_KEY = 'm6cvwnsyi!59_+^32q)=hwrp!z%r24jri5rd==pkwaf*%7b#aa'
 DEBUG = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 # ALLOWED_HOSTS = ['andrewpear-goldenkey-django-e550.twc1.net', 'localhost', 'andrewpear-goldenkey-django-0615.twc1.net']
 ALLOWED_HOSTS = ['*']
 # if (request.build_absolute_uri('/') == 'localhost'):
-# SECURE_SSL_REDIRECT = True
 
 CSRF_TRUSTED_ORIGINS=["https://andrewpear-goldenkey-django-e550.twc1.net"]
 CSRF_COOKIE_SECURE = True
